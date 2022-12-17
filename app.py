844a32fbed5a6a47ad977e69e86aa214b87c4377
Colin FitzGerald <colinfitzgerald328@gmail.com>
2022-12-17 14:40:43 -0800

    writing newest route

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, request
+from utilities import running_pace_calculator
 
 app = Flask(__name__)
 
@@ -8,3 +9,20 @@ def index():
 	return "Hello World!"
 
 
+@app.route("/convert")
+def convert_input():
+	running_time = str(request.args.get("running_time", "")).strip()
+	running_distance = str(request.args.get("running_distance", "")).strip()
+	running_pace = str(request.args.get("running_pace")).strip()
+	if running_time and running_distance != "":
+		final_output = running_pace_calculator(running_time, running_distance, running_pace)
+	elif running_distance and running_pace != "":
+		final_output = running_pace_calculator(running_time, running_distance, running_pace)
+	elif running_time and running_pace != "":
+		final_output = running_pace_calculator(running_time, running_distance, running_pace)
+	else:
+		final_output = ""
+	return {
+			"operation": "success",
+			"converted_result": final_output
+	}
